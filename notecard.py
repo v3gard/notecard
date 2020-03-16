2ccc8b3e56eb9c1c346441ee036c6456ebe8a21a
Vegard Haugland <vegard@Vegards-MacBook-Pro.local>
2020-03-16 22:46:16 +0100

    Almost good enough for initial release.

--- notecard.py
+++ notecard.py
@@ -3,11 +3,11 @@ import pygame.midi
 import sys
 import math
 import random
+import os
 from pygame.locals import *
 from enum import IntEnum
 
 WHITE = (255,255,255)
-RED = (255,0,0)
 BLACK = (0,0,0)
 
 class KeyType(IntEnum):
@@ -40,53 +40,131 @@ class Key(object):
 class Note(pygame.sprite.Sprite):
     def __init__(self, color, radius, key):
         super().__init__()
+        self.hidden = False
         self.image = pygame.Surface([100, 100])
         self.image.fill(WHITE)
         self.image.set_colorkey(WHITE)
-        WIDTH=24
-        HEIGHT=18
-        OFFSET=40
-        LINEHEIGHT=40
-        LINEOFFSET=3
-        pygame.draw.ellipse(self.image, color, (OFFSET,OFFSET,WIDTH,HEIGHT))
-        pygame.draw.line(self.image, color, (OFFSET+WIDTH+(LINEOFFSET*-1),OFFSET+LINEOFFSET), (OFFSET+WIDTH+(LINEOFFSET*-1),OFFSET-LINEHEIGHT+LINEOFFSET))
+        self.color = color
+        self.WIDTH=24
+        self.HEIGHT=18
+        self.OFFSET=40
+        self.LINEHEIGHT=40
+        self.LINEOFFSET=3
+        self.sharp = False
+        pygame.draw.ellipse(self.image, self.color, (self.OFFSET,self.OFFSET,self.WIDTH,self.HEIGHT))
+        self.drawUpline = False
+        self.drawDownline = False
+        self.linesDrawn = False
         self.rect = self.image.get_rect()
-        self.rect.x = 200
-        if key.keytype.name == "c" and key.octave == 5:
-            y = OFFSET+HEIGHT/2
-            x = OFFSET-4
-            pygame.draw.line(self.image, color, (x, y), (x+WIDTH+8, y))
+        self.rect.x = 250
+        if "sh" in key.keytype.name:
+            self.sharp = True
+        if key.keytype.name in ("c","csh") and key.octave == 5:
+            y = self.OFFSET+self.HEIGHT/2
+            x = self.OFFSET-4
+            pygame.draw.line(self.image, self.color, (x, y), (x+self.WIDTH+8, y))
+            self.drawUpline = True
             self.rect.y = 150
-        elif key.keytype.name == "d" and key.octave == 5:
+        elif key.keytype.name in ("d","dsh") and key.octave == 5:
             self.rect.y = 140
-        elif key.keytype.name == "e" and key.octave == 5:
+            self.drawUpline = True
+        elif key.keytype.name in ("e") and key.octave == 5:
             self.rect.y = 133
-        elif key.keytype.name == "f" and key.octave == 5:
+            self.drawUpline = True
+        elif key.keytype.name in ("f","fsh") and key.octave == 5:
             self.rect.y = 122
-        elif key.keytype.name == "g" and key.octave == 5:
+            self.drawUpline = True
+        elif key.keytype.name in ("g","gsh") and key.octave == 5:
             self.rect.y = 112
-        elif key.keytype.name == "a" and key.octave == 5:
+            self.drawUpline = True
+        elif key.keytype.name in ("a","ash") and key.octave == 5:
             self.rect.y = 102
-        elif key.keytype.name == "b" and key.octave == 5:
+            self.drawUpline = True
+        elif key.keytype.name in ("b") and key.octave == 5:
             self.rect.y = 92
-        elif key.keytype.name == "c" and key.octave == 6:
+            self.drawDownline = True
+        elif key.keytype.name in ("c","csh") and key.octave == 6:
             self.rect.y = 82
-        elif key.keytype.name == "d" and key.octave == 6:
+            self.drawDownline = True
+        elif key.keytype.name in ("d","dsh") and key.octave == 6:
             self.rect.y = 72
-        elif key.keytype.name == "e" and key.octave == 6:
+            self.drawDownline = True
+        elif key.keytype.name in ("e") and key.octave == 6:
             self.rect.y = 62
-        elif key.keytype.name == "f" and key.octave == 6:
+            self.drawDownline = True
+        elif key.keytype.name in ("f","fsh") and key.octave == 6:
             self.rect.y = 52
-        elif key.keytype.name == "g" and key.octave == 6:
+            self.drawDownline = True
+        elif key.keytype.name in ("g","gsh") and key.octave == 6:
             self.rect.y = 42
-        elif key.keytype.name == "a" and key.octave == 6:
+            self.drawDownline = True
+        elif key.keytype.name in ("a","ash") and key.octave == 6:
             self.rect.y = 32
-        elif key.keytype.name == "b" and key.octave == 6:
+            self.drawDownline = True
+        elif key.keytype.name in ("b") and key.octave == 6:
             self.rect.y = 22
+            self.drawDownline = True
+        elif key.keytype.name in ("c","csh") and key.octave == 7:
+            self.rect.y = 12
+            self.drawDownline = True
+        elif key.keytype.name in ("b") and key.octave == 4:
+            self.rect.y = 242
+            self.drawDownline = True
+        elif key.keytype.name in ("a","ash") and key.octave == 4:
+            self.rect.y = 252
+            self.drawDownline = True
+        elif key.keytype.name in ("g","gsh") and key.octave == 4:
+            self.rect.y = 262
+            self.drawDownline = True
+        elif key.keytype.name in ("f","fsh") and key.octave == 4:
+            self.rect.y = 272
+            self.drawDownline = True
+        elif key.keytype.name in ("e") and key.octave == 4:
+            self.rect.y = 282
+            self.drawDownline = True
+        elif key.keytype.name in ("d","dsh") and key.octave == 4:
+            self.rect.y = 292
+            self.drawDownline = True
+        elif key.keytype.name in ("c","csh") and key.octave == 4:
+            self.rect.y = 302
+            self.drawDownline = True
+        elif key.keytype.name in ("b") and key.octave == 3:
+            self.rect.y = 312
+            self.drawDownline = True
+        elif key.keytype.name in ("a","ash") and key.octave == 3:
+            self.rect.y = 322
+            self.drawDownline = True
+        elif key.keytype.name in ("g","gsh") and key.octave == 3:
+            self.rect.y = 332
+            self.drawDownline = True
+        elif key.keytype.name in ("f","fsh") and key.octave == 3:
+            self.rect.y = 342
+            self.drawDownline = True
+        elif key.keytype.name in ("e") and key.octave == 3:
+            self.rect.y = 352
+            self.drawDownline = True
+        elif key.keytype.name in ("d","dsh") and key.octave == 3:
+            self.rect.y = 362
+            self.drawDownline = True
+        elif key.keytype.name in ("c","csh") and key.octave == 3:
+            self.rect.y = 372
+            self.drawDownline = True
         else:
-            self.rect.x = random.randint(0,100)
-            self.rect.y = random.randint(0,100)
-        
+            self.hidden = True
+
+        if self.sharp:
+            font = pygame.font.Font('freesansbold.ttf', 32) 
+            text = font.render('#', True, self.color, None)
+            textRect = text.get_rect() 
+            textRect.center = (50+self.WIDTH, 60-(self.HEIGHT)) 
+            self.image.blit(text, textRect)
+    
+    def show_lines(self):
+        if self.linesDrawn == False:
+            if self.drawUpline:
+                 pygame.draw.line(self.image, self.color, (self.OFFSET+self.WIDTH+(self.LINEOFFSET*-1),self.OFFSET+self.LINEOFFSET), (self.OFFSET+self.WIDTH+(self.LINEOFFSET*-1),self.OFFSET-self.LINEHEIGHT+self.LINEOFFSET))
+            if self.drawDownline:
+                pygame.draw.line(self.image, self.color, (self.OFFSET+(self.LINEOFFSET),self.OFFSET+self.LINEOFFSET), (self.OFFSET+(self.LINEOFFSET),self.OFFSET+self.LINEHEIGHT+self.LINEOFFSET))
 
 def render_notes(notes, raw_keys):
     for note in notes:
@@ -94,17 +172,20 @@ def render_notes(notes, raw_keys):
     keys = [Key(x) for x in raw_keys]
     for key in keys:
         note = Note(BLACK, 10, key)
-        notes.add(note)
-    print(keys)
+        if not note.hidden:
+            if len(keys) == 1:
+                note.show_lines()
+            notes.add(note)
 
-def draw_clefs(screen):
+def draw_clefs(screen, midi, gclef, fclef):
     screen.fill(WHITE)
-    # treble clef
+
     pygame.draw.line(screen, BLACK, [100,100], [500,100], 1)
     pygame.draw.line(screen, BLACK, [100,120], [500,120], 1)
     pygame.draw.line(screen, BLACK, [100,140], [500,140], 1)
     pygame.draw.line(screen, BLACK, [100,160], [500,160], 1)
     pygame.draw.line(screen, BLACK, [100,180], [500,180], 1)
+    screen.blit(gclef, (90,47))
     
     # bass clef
     VPOS_BASS = 300
@@ -113,28 +194,36 @@ def draw_clefs(screen):
     pygame.draw.line(screen, BLACK, [100,VPOS_BASS + 40], [500,VPOS_BASS + 40], 1)
     pygame.draw.line(screen, BLACK, [100,VPOS_BASS + 60], [500,VPOS_BASS + 60], 1)
     pygame.draw.line(screen, BLACK, [100,VPOS_BASS + 80], [500,VPOS_BASS + 80], 1)
+    screen.blit(fclef, (90,243))
 
 pygame.init()
 pygame.midi.init()
+pygame.font.init()
 
 # List all midi devices
+midi=True
 mididevices = pygame.midi.get_count()
 if (mididevices == 0):
     print("No MIDI devices found.")
-    exit(0)
-for x in range(0, mididevices):
-    print(pygame.midi.get_device_info(x))
-inp = pygame.midi.Input(0)
+    midi=False
 
-WIDTH = 800
-HEIGHT = 600
+if midi:
+    inp = pygame.midi.Input(0)
+
+WIDTH = 640
+HEIGHT = 480
 TITLE = "Notecard"
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
-refresh_rate = 60
+refresh_rate = 60 
 
-draw_clefs(screen)
+trebleImg = None
+bassImg = None
+gclef = pygame.image.load(os.path.join('trebleclef.png'))
+fclef = pygame.image.load(os.path.join('bassclef.png'))
+
+draw_clefs(screen, midi, gclef, fclef)
 
 pygame.display.flip()
 pygame.display.update()
@@ -148,7 +237,7 @@ KEY_RELEASED = 128
 
 done = False
 while not done:
-    if inp.poll():
+    if midi and inp.poll():
         midievent = inp.read(1000)
         for event in midievent:
             state, key, _, _ = event[0]
@@ -161,10 +250,11 @@ while not done:
         render_notes(notes, keys_pressed)
     for event in pygame.event.get():
         if event.type == QUIT:
-            pygame.quit()
-            exit(0)
-    draw_clefs(screen)
+            done = True
+    draw_clefs(screen, midi, gclef, fclef)
     notes.draw(screen)
     pygame.display.update()
     clock.tick(refresh_rate)
+if midi:
+    inp.close()
 pygame.quit()
